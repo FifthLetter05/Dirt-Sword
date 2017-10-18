8cf83a0089f19195cf90ad446021ca6c2313e7d3
Ian Kraybill <iek2day@gmail.com>
2017-10-18 16:58:46 -0700

    cleaned up code and ensured exact entity copying

--- src/main/java/com/ikraybill/dirtsword/item/ItemDirtSword.java
+++ src/main/java/com/ikraybill/dirtsword/item/ItemDirtSword.java
@@ -1,26 +1,13 @@
 package com.ikraybill.dirtsword.item;
 
 import com.ikraybill.dirtsword.reference.Reference;
-import com.ikraybill.dirtsword.utility.LogHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.monster.EntitySkeleton;
-import net.minecraft.entity.monster.EntityZombie;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
-import static com.ikraybill.dirtsword.utility.Utility.copyEntity;
 
 public class ItemDirtSword extends ItemSwordMT{
 
@@ -37,18 +24,30 @@ public class ItemDirtSword extends ItemSwordMT{
     @Override
     public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
         World world = target.worldObj;
-        target.setHealth(0);
         if(!world.isRemote){
-            //LogHelper.info(target.getClass());
+            //LogHelper.info();
             EntityLivingBase targetCopy[] = new EntityLivingBase[2];
             for (int i = 0; i < 2; i++) {
                 targetCopy[i] = copyEntity(target, world);
                 world.spawnEntityInWorld(targetCopy[i]);
             }
         }
+        target.setHealth(0);
         if (!(attacker instanceof EntityPlayer && ((EntityPlayer) attacker).capabilities.isCreativeMode)) {
             stack.damageItem(2, attacker);
         }
         return super.hitEntity(stack, target, attacker);
+    }
+
+    private EntityLivingBase copyEntity(EntityLivingBase entity, World worldIn){
+
+        Entity entityCopyBase = EntityList.createEntityByName(entity.getName(), worldIn);
+        EntityLivingBase entityCopy = (EntityLivingBase) entityCopyBase;
+        assert entityCopy != null;
+        entityCopy.readEntityFromNBT(entity.serializeNBT());
+        entityCopy.setPosition(entity.posX, entity.posY,entity.posZ);
+        entityCopy.setHealth(entity.getMaxHealth());
+
+        return entityCopy;
     }
 }
